817891d9afc82fc060d30b4bc1961a0a2ef32164
bmaorlo <maor.bolo@gmail.com>
2020-06-16 11:16:35 +0300

    Delete not needed

--- storage/framework/views/029db41c60869bd2bdade4b8331e6bc4b942e068.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            
-        </div>
-    </div>
-</footer><?php /**PATH C:\xampp\htdocs\deals\resources\views\includes\dashboard\footer.blade.php ENDPATH**/ ?>
