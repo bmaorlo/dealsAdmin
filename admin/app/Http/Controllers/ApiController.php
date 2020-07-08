53902cdfca6d2afea623bc1482daf25aac8e0499
bmaorlo <maor.bolo@gmail.com>
2020-07-08 11:15:50 +0300

    Fix error

--- admin/app/Http/Controllers/ApiController.php
+++ admin/app/Http/Controllers/ApiController.php
@@ -59,7 +59,7 @@ class ApiController extends Controller
                 $response['items'][$i]['themes'][$theme->theme->id]=$theme->theme->name;
             }
 
-
+            $response['items'][$i]['deals'] = array();
             foreach ($deals as $deal){
 
                 array_push($response['items'][$i]['deals'],[
